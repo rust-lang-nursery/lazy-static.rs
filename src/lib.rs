86d96a41de993154fbe3a199fde02ec31660ad34
Marvin Löbel <loebel.marvin@gmail.com>
2017-04-12 11:54:02 +0200

    Dual license under both MIT and APACHE-2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// Copyright 2016 lazy-static.rs Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 /*!
 A macro for declaring lazily evaluated statics.
 
