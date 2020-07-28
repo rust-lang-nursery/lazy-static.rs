0e80ae292d3f34aed67d9c9686398a6aecdd1351
Travis CI User <travis@example.org>
2020-07-28 22:38:53 +0000

    Documentation for rust-lang-nursery/lazy-static.rs@f32efad

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"lazy_static":{"doc":"A macro for declaring lazily evaluated statics.","i":[[5,"initialize","lazy_static","Takes a shared reference to a lazy static and initializes…",null,[[]]],[8,"LazyStatic","","Support trait for enabling a few common operation on lazy…",null,null],[14,"lazy_static","","",null,null]],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["lazy_static"] = {"name":"","files":["core_lazy.rs","lib.rs"]};
+createSourceSidebar();
